959704485b284ac8362443a90d4718235b2745e1
codewizai <alekiran22.softeng@gmail.com>
2026-04-06 14:10:51 +1000

    change is databse configuration for railway deployment

--- config/database.js
+++ config/database.js
@@ -1,12 +1,23 @@
+// FOR LOCAL
+// import mysql from 'mysql2/promise';
+// import dotenv from 'dotenv';
+// dotenv.config();
+
+// const pool = mysql.createPool({
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME
+// });
+
+// export default pool;
+
+// FOR RAILWAY
 import mysql from 'mysql2/promise';
 import dotenv from 'dotenv';
+
 dotenv.config();
 
-const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-});
+const pool = mysql.createPool(process.env.DATABASE_URL);
 
 export default pool;
