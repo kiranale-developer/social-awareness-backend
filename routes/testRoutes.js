3290c2be40f6af31bf6135fbd421c8774d412f8c
codewizai <alekiran22.softeng@gmail.com>
2026-04-06 22:10:45 +1000

    change in db to pool for cloud integration

--- routes/testRoutes.js
+++ routes/testRoutes.js
@@ -1,9 +1,11 @@
+import pool from "../config/database.js"
 import express from "express";
+
 const router = express.Router();
 
 router.get("/", async (req, res) => {
   try {
-    const [rows] = await db.query("SELECT 1 AS test");
+    const [rows] = await pool.query("SELECT 1 AS test");
     res.json({ message: "DB connected ✅", rows });
   } catch (error) {
     res.status(500).json({ error: error.message });
