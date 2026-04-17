51aeb8d3b55d1e7f8cd279faccf1b1c414cdc7cf
kiranale-developer <60783459+kiranale-developer@users.noreply.github.com>
2026-04-17 19:58:17 +1000

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import cors from 'cors';
+import pool from "../config/database.js";
 
 import authRoutes from './routes/authRoutes.js';
 import campaignRoutes from './routes/campaignRoutes.js';
