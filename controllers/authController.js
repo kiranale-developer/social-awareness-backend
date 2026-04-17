72119cd280c6fb924bb58309f1f49992d7d54fc8
kiranale-developer <60783459+kiranale-developer@users.noreply.github.com>
2026-04-17 19:14:58 +1000

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -128,14 +128,14 @@ export const login = async (req, res) => {
     ]);
 
     if (check.length === 0) {
-      return res.status(400).json({ message: 'Invalid Credentails' });
+      return res.status(400).json({ message: 'Invalid redentails' });
     }
 
     const user = check[0];
 
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
-      return res.status(400).json({ message: 'Invalid Credentails' });
+      return res.status(400).json({ message: 'Invalid Creentails' });
     }
     const accessToken = generateAccessToken(user);
     const refreshToken = generateRefreshToken(user);
