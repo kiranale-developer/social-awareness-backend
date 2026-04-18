ffaa76e3e36cffb75766e8afa28fb1f3efd5bdcc
kiranale-developer <60783459+kiranale-developer@users.noreply.github.com>
2026-04-18 21:28:55 +1000

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,31 +1,36 @@
-// // FOR LOCAL
-// import mysql from 'mysql2/promise';
-// import dotenv from 'dotenv';
-// dotenv.config();
-
-// const pool = mysql.createPool({
-//   host: process.env.DB_HOST,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_NAME,
-// });
-
-// export default pool;
-
-//FOR RAILWAY
 import mysql from 'mysql2/promise';
-// import dotenv from 'dotenv';
 
-// dotenv.config();
-
-const pool = mysql.createPool({
-  host: process.env.MYSQLHOST,
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
-  port: Number(process.env.MYSQLPORT),
-});
-
-
+const pool = mysql.createPool(process.env.DATABASE_URL);
 
 export default pool;
+// // // FOR LOCAL
+// // import mysql from 'mysql2/promise';
+// // import dotenv from 'dotenv';
+// // dotenv.config();
+
+// // const pool = mysql.createPool({
+// //   host: process.env.DB_HOST,
+// //   user: process.env.DB_USER,
+// //   password: process.env.DB_PASSWORD,
+// //   database: process.env.DB_NAME,
+// // });
+
+// // export default pool;
+
+// //FOR RAILWAY
+// import mysql from 'mysql2/promise';
+// // import dotenv from 'dotenv';
+
+// // dotenv.config();
+
+// const pool = mysql.createPool({
+//   host: process.env.MYSQLHOST,
+//   user: process.env.MYSQLUSER,
+//   password: process.env.MYSQLPASSWORD,
+//   database: process.env.MYSQLDATABASE,
+//   port: Number(process.env.MYSQLPORT),
+// });
+
+
+
+// export default pool;
